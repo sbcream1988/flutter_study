9682a239542e6e76f1f2001f5076182bc70c0041
sbcream1988 <104984824+sbcream1988@users.noreply.github.com>
2023-02-18 11:21:56 +0900

    Delete lib directory

--- lib/presentation/home_ui_event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'home_ui_event.freezed.dart';
-
-@freezed
-abstract class HomeUiEvent with _$HomeUiEvent {
-  factory HomeUiEvent.showsnackbar(String message) = ShowSnackBar;
-}
